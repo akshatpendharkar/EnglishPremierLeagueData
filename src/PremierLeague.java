7611303a17597272ef547427d704458e7bcea7a9
ap3ca <ap3ca@users.noreply.github.com>
2017-05-28 17:58:19 -0400

    initial project

---
+++ src/PremierLeague.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PremierLeague {
+	
+	public static void main(String[] args) throws InterruptedException, IOException {
+		
+		ArrayList<String> filePaths = readAllPaths();
+		System.out.println(filePaths.size());
+				
+		PremierLeagueRunner runner = new PremierLeagueRunner(filePaths, 0, filePaths.size());
+		Thread thread = new Thread(runner);
+		thread.start();		
+			
+		thread.join();
+		System.out.println(Arrays.asList(runner.getReadings()));
+		
+		ArrayList<String> merged = new ArrayList<String>();
+		mergeCounts(runner.getReadings(), merged);
+
+		writeCSV(merged);	
+		
+	}
+  
+	private static ArrayList<String> readAllPaths() {
+		System.out.println("readAllPaths");
+		ArrayList<String> paths = new ArrayList<String>();
+		try{
+			Files.walk(Paths.get("C:\\Users\\Akshat\\Desktop\\EPL_Data_1992_2015")).filter(Files::isRegularFile).map(path -> path.toString()).forEach(paths::add);		
+		}
+		catch (IOException exception) {
+			exception.printStackTrace();
+		}
+		return paths;		
+	}
+	
+	private static void mergeCounts (ArrayList<String> runnerCount, ArrayList<String> mergeCount){
+		System.out.println("mergeCounts");
+		for (String d : runnerCount){
+			mergeCount.add(d);
+		}
+	}
+	
+	private static void writeCSV (ArrayList<String> combined) throws FileNotFoundException {
+		System.out.println("writeCSV");
+		File csvFile = new File("C:\\Users\\Akshat\\Desktop\\PremierLeague.csv");
+		csvFile.getParentFile().mkdirs();
+		
+		PrintWriter writer = new PrintWriter(csvFile);
+		writer.write("season,rank,team,played,wins,draws,losses,goals_for,goals_against,goals_difference,points");
+		
+		for (String d : combined) {
+			writer.write("\n" + d);
+		}
+		writer.close();
+		System.out.println("finished");
+	}
+	
+}
---
+++ src/PremierLeagueRunner.java
@@ -0,0 +1,68 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+public class PremierLeagueRunner implements Runnable{
+	private ArrayList<String> filePaths;
+	private int start;
+	private int end;
+	private ArrayList<String> readings;
+	
+	public ArrayList<String> getReadings()
+	{
+		return readings;
+	}
+
+	public PremierLeagueRunner(ArrayList<String> filePaths, int start, int end) {
+		this.filePaths = filePaths;
+		this.start = start;
+		this.end = end;
+	}
+	
+	public void run() {
+		this.readings = new ArrayList<String>();
+
+		for (int i = start; i < end; i++) {
+			FileReader jsonFile = null;
+			
+			try {
+				jsonFile = new FileReader(filePaths.get(i));
+			}
+			catch (FileNotFoundException exp) {
+				exp.printStackTrace();
+			}
+			
+			JsonParser jparser = new JsonParser();
+			JsonArray jarray = jparser.parse(jsonFile).getAsJsonArray();
+			
+			for (int j = 0; j < jarray.size(); j++) {
+				JsonObject jobject = (JsonObject) jarray.get(j);
+				String yearStart = String.valueOf(1992 + j);	
+				String yearEnd = String.valueOf(1992 + j + 1);	
+				String season = yearStart + "-" + yearEnd;
+				String rank = jobject.get("rank").getAsString();
+				String team = jobject.get("team").getAsString();
+				String played = jobject.get("played").getAsString();
+				String wins = jobject.get("wins").getAsString();
+				String draws = jobject.get("draws").getAsString();
+				String losses = jobject.get("losses").getAsString();
+				String goals_for = jobject.get("goals-for").getAsString();
+				String goals_against = jobject.get("goals-against").getAsString();
+				String goals_difference = jobject.get("goals-dff").getAsString();
+				String points = jobject.get("points").getAsString();
+				
+				String data = season + "," + rank + "," + team + "," + 
+						played + "," + wins + "," + draws + "," + losses + "," + 
+						goals_for + "," + goals_against + "," + goals_difference 
+						+ "," + points;
+				
+				readings.add(data);
+				
+			}	
+		}
+	}
+		
+}
